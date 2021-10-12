6e3ab82fcb59fd1f210968bb5e7e529540793b46
Kelly Anne Pipe <kellyanne.pipe@gmail.com>
2021-10-12 12:09:05 -0400

    Linting

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,7 +1,6 @@
 import { Meta } from "../layout/Meta";
 import { Section } from "../layout/Section";
 import { AppConfig } from "../utils/AppConfig";
-import { Banner } from "./Banner";
 import { Footer } from "./Footer";
 import { Hero } from "./Hero";
 import Link from "next/link";
