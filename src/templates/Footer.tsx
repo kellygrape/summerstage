7f84e5cd7ccc49592e423a68f68df6648709d465
Kelly Anne Pipe <kellyanne.pipe@gmail.com>
2021-10-12 12:17:32 -0400

    Make the form its own component

--- src/templates/Footer.tsx
+++ src/templates/Footer.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { Background } from "../background/Background";
+import Form from "../form/Form";
 import { Section } from "../layout/Section";
 
 const Footer = () => (
@@ -12,131 +14,78 @@ const Footer = () => (
             </h1>
           </div>
           <div className="lg:w-1/2 md:w-2/3 mx-auto">
-            <form name="contact" method="POST" data-netlify="true">
-              <div className="flex flex-wrap -m-2">
-                <div className="p-2 w-1/2">
-                  <div className="relative">
-                    <label
-                      htmlFor="name"
-                      className="leading-7 text-sm text-gray-600"
-                    >
-                      Name
-                    </label>
-                    <input
-                      type="text"
-                      id="name"
-                      name="name"
-                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                    />
-                  </div>
-                </div>
-                <div className="p-2 w-1/2">
-                  <div className="relative">
-                    <label
-                      htmlFor="email"
-                      className="leading-7 text-sm text-gray-600"
-                    >
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                    />
-                  </div>
-                </div>
-                <div className="p-2 w-full">
-                  <div className="relative">
-                    <label
-                      htmlFor="message"
-                      className="leading-7 text-sm text-gray-600"
-                    >
-                      Message
-                    </label>
-                    <textarea
-                      id="message"
-                      name="message"
-                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                    ></textarea>
-                  </div>
-                </div>
-                <div className="p-2 w-full">
-                  <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                    Button
-                  </button>
-                </div>
-                <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+            <div className="flex flex-wrap -m-2">
+              <Form />
+              <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+                <a
+                  className="text-indigo-500"
+                  href="mailto:friendsofudsummerstage@gmail.com"
+                >
+                  friendsofudsummerstage@gmail.com
+                </a>
+                <p className="leading-normal my-5">
+                  P.O.Box 707
+                  <br />
+                  Drexel Hill, PA 19026
+                  <br />
+                  Phone: 610-394-1580
+                </p>
+                <span className="inline-flex">
                   <a
-                    className="text-indigo-500"
-                    href="mailto:friendsofudsummerstage@gmail.com"
+                    className="text-gray-500"
+                    href="https://www.facebook.com/UDSummerStage"
                   >
-                    friendsofudsummerstage@gmail.com
+                    <svg
+                      fill="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                    </svg>
                   </a>
-                  <p className="leading-normal my-5">
-                    P.O.Box 707
-                    <br />
-                    Drexel Hill, PA 19026
-                    <br />
-                    Phone: 610-394-1580
-                  </p>
-                  <span className="inline-flex">
-                    <a
-                      className="text-gray-500"
-                      href="https://www.facebook.com/UDSummerStage"
+                  <a
+                    className="ml-4 text-gray-500"
+                    href="https://www.youtube.com/c/UDPACmagic"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
                     >
-                      <svg
-                        fill="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        className="w-5 h-5"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                      </svg>
-                    </a>
-                    <a
-                      className="ml-4 text-gray-500"
-                      href="https://www.youtube.com/c/UDPACmagic"
+                      <path d="M10 9.333l5.333 2.662-5.333 2.672v-5.334zm14-4.333v14c0 2.761-2.238 5-5 5h-14c-2.761 0-5-2.239-5-5v-14c0-2.761 2.239-5 5-5h14c2.762 0 5 2.239 5 5zm-4 7c-.02-4.123-.323-5.7-2.923-5.877-2.403-.164-7.754-.163-10.153 0-2.598.177-2.904 1.747-2.924 5.877.02 4.123.323 5.7 2.923 5.877 2.399.163 7.75.164 10.153 0 2.598-.177 2.904-1.747 2.924-5.877z" />
+                    </svg>
+                  </a>
+                  <a
+                    href="http://instagram.com/UDSummerStage"
+                    className="ml-4 text-gray-500"
+                  >
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
                     >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-5 h-5"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                      >
-                        <path d="M10 9.333l5.333 2.662-5.333 2.672v-5.334zm14-4.333v14c0 2.761-2.238 5-5 5h-14c-2.761 0-5-2.239-5-5v-14c0-2.761 2.239-5 5-5h14c2.762 0 5 2.239 5 5zm-4 7c-.02-4.123-.323-5.7-2.923-5.877-2.403-.164-7.754-.163-10.153 0-2.598.177-2.904 1.747-2.924 5.877.02 4.123.323 5.7 2.923 5.877 2.399.163 7.75.164 10.153 0 2.598-.177 2.904-1.747 2.924-5.877z" />
-                      </svg>
-                    </a>
-                    <a
-                      href="http://instagram.com/UDSummerStage"
-                      className="ml-4 text-gray-500"
-                    >
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        className="w-5 h-5"
-                        viewBox="0 0 24 24"
-                      >
-                        <rect
-                          width="20"
-                          height="20"
-                          x="2"
-                          y="2"
-                          rx="5"
-                          ry="5"
-                        ></rect>
-                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                      </svg>
-                    </a>
-                  </span>
-                </div>
+                      <rect
+                        width="20"
+                        height="20"
+                        x="2"
+                        y="2"
+                        rx="5"
+                        ry="5"
+                      ></rect>
+                      <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                    </svg>
+                  </a>
+                </span>
               </div>
-            </form>
+            </div>
           </div>
         </div>
       </section>
